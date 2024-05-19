75dc424fffe5c845bd22e9e3064ef3fb59b16779
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-05-19 09:10:13 +0530

    made circuit diagram cleaner
    Signed-off-by: Roopesh O R <62459820+Roopesh2@users.noreply.github.com>

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/bash
 
-rm **/*.aux \
- **/*.fls \
- **/*.log \
- **/*.out \
- **/*.ps \
- **/*.maf \
- **/*.mtc \
- **/*.mtc0 \
- **/*.synctex.gz \
- **/*.fdb_latexmk \
- **/_minted-* -r
+rm **/**/*.aux \
+ **/**/*.fls \
+ **/**/*.log \
+ **/**/*.out \
+ **/**/*.ps \
+ **/**/*.maf \
+ **/**/*.mtc \
+ **/**/*.mtc0 \
+ **/**/*.synctex.gz \
+ **/**/*.fdb_latexmk \
+ */**/*.pdf \
+ **/**/_minted-* \
+ **/svg-inkscape -r
