4f6eaf8619995a7899bcd3cfca7efddd5c48f554
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-06-11 22:01:55 +0530

    recompiled & updated cleanup script
    Signed-off-by: Roopesh O R <62459820+Roopesh2@users.noreply.github.com>

--- cleanup.sh
+++ cleanup.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/bash
 
-rm **/*.aux \
- **/*.fls \
- **/*.log \
- **/*.out \
- **/*.ps \
- **/*.maf \
- **/*.mtc \
- **/*.mtc0 \
- **/*.synctex.gz \
- **/*.fdb_latexmk \
- **/*.pdf \
- **/*.xdv \
+rm **/**/*.aux \
+ **/**/*.fls \
+ **/**/*.log \
+ **/**/*.out \
+ **/**/*.ps \
+ **/**/*.maf \
+ **/**/*.mtc \
+ **/**/*.mtc0 \
+ **/**/*.synctex.gz \
+ **/**/*.fdb_latexmk \
+ **/*/*.pdf \
+ **/**/*.xdv \
  **/_minted-* \
  **/svg-inkscape -r
