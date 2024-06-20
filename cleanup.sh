1543598619ca0ad99c4cad0dc17a0c12be65e477
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-06-20 19:24:25 +0530

    Moved tex files to respective modules
    Signed-off-by: Roopesh O R <62459820+Roopesh2@users.noreply.github.com>

--- cleanup.sh
+++ cleanup.sh
@@ -1,16 +1,15 @@
 #! /usr/bin/bash
 
-rm **/**/*.aux \
- **/**/*.fls \
- **/**/*.log \
- **/**/*.out \
- **/**/*.ps \
- **/**/*.maf \
- **/**/*.mtc \
- **/**/*.mtc0 \
- **/**/*.synctex.gz \
- **/**/*.fdb_latexmk \
- **/*/*.pdf \
- **/**/*.xdv \
- **/_minted-* \
- **/svg-inkscape -r
+ find . -name "*.aux" -delete
+ find . -name "*.fls" -delete
+ find . -name "*.log" -delete
+ find . -name "*.out" -delete
+ find . -name "*.ps" -delete
+ find . -name "*.maf" -delete
+ find . -name "*.mtc" -delete
+ find . -name "*.mtc0" -delete
+ find . -name "*.synctex.gz" -delete
+ find . -name "*.fdb_latexmk" -delete
+ find . -name "*.xdv" -delete
+ find . -name "*_minted-*" -delete
+ find . -name "*svg-inkscape" -delete
