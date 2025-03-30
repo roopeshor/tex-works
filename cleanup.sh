c86b5dcaef2aa475219c10dc3a90c22597664148
Roopesh O R <62459820+roopeshor@users.noreply.github.com>
2025-03-30 17:33:08 +0530

    added miniproject
    Signed-off-by: Roopesh O R <62459820+roopeshor@users.noreply.github.com>

--- cleanup.sh
+++ cleanup.sh
@@ -9,6 +9,9 @@
  find . -name "*.maf" -delete
  find . -name "*.mtc" -delete
  find . -name "*.mtc0" -delete
+ find . -name "*.toc" -delete
+ find . -name "*.lof" -delete
+ find . -name "*.lot" -delete
  find . -name "*.synctex.gz" -delete
  find . -name "*.fdb_latexmk" -delete
  find . -name "*.xdv" -delete
